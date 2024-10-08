fbff7800b7d0677efc3a466c1d041fd29c36ead6
Chhaya <chayakudas.das56@gmail.com>
2024-10-08 20:11:56 +0530

    Adding training prep utils

--- multigpu_train.py
+++ multigpu_train.py
@@ -47,3 +47,56 @@ class TrainerConfig:
     snapshot_path: Optional[str] = None
     save_every: int = None
     use_amp: bool = None
+
+@dataclass
+class Snapshot:
+    model_state: 'OrderedDict[str, torch.Tensor]'
+    optimizer_state: Dict[str, Any]
+    finished_epoch: int
+
+
+class Trainer:
+
+    def __init__(self, cfg: TrainerConfig, model, optimizer, train_dataset, test_dataset=None):
+
+        self.cfg = cfg
+        self.local_rank = int(os.environ['LOCAL_RANK'])
+        self.global_rank = int(os.environ['RANK'])
+
+        self.train_dataset = train_dataset
+        
+    def _prepare_dataloader(self, dataset: Dataset):
+        return Dataloader(
+            dataset,
+            batch_size=self.cfg.batch_size,
+            pin_memory=True,
+            shuffle=False,
+            num_workers=self.cfg.data_loader_workers,
+            sampler=DistributedSampler(dataset),
+        )
+
+    def _load_snapshot(self):
+        try:
+            snapshot_data = torch.load(self.cfg.snapshot_path, map_location="cpu")
+        except FileNotFoundError:
+            print("Snapshot was not found. Training model from scratch")
+            return
+        
+        snapshot = Snapshot(**snapshot_data)
+        self.model.load_state_dict(snapshot.model_state)
+        self.optimizer.load_state_dict(snapshot.optimizer_state)
+        self.epochs_run = snapshot.finished_epoch
+        print("Resuming training from snapshot at epoch:", self.epochs_run)
+
+    def _save_snapshot(self, epoch):
+        model = self.model
+        raw_model = model.module if hasattr(model, "module") else model
+        snapshot = Snapshot(
+            model_state=raw_model.state_dict(),
+            optimizer_state=self.optimizer.state_dict(),
+            finished_epoch=epoch,
+        )
+
+        snapshot = asdict(snapshot)
+        torch.save(snapshot, self.config.snapshot_path)
+        print("Snapshot saved at epoch:", epoch)

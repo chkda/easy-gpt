e63ef4b2b24b84f9d2d8c0e8e143e404080080aa
Chhaya <chayakudas.das56@gmail.com>
2024-10-10 15:27:59 +0530

    minor typo

--- multigpu_train.py
+++ multigpu_train.py
@@ -140,7 +140,7 @@ class Trainer:
         
         return loss.item()
 
-    def _run_epoch(self, epochs: int, dataloader: DataLoader, train: bool = True):
+    def _run_epoch(self, epoch: int, dataloader: DataLoader, train: bool = True):
         dataloader.sampler.set_epoch(epoch)
         for i, (source, targets) in enumerate(dataloader):
             step_type = "Train" if train else "Eval"
